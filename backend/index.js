337cd36041306298aa9a29c7c35c97269b3bb3ab
Sunny Gupta <sunnyroy8084401051@example.com>
2025-08-27 08:01:58 +0530

    change in entry file

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,6 @@ app.get("/", (req, res) => {
   res.send("server is running ");
 });
 
-app.listen(port, () => {
-  console.log(`server is running on => ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`server is running on => ${port}`);
+// });
